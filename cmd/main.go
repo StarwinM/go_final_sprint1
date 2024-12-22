9f4501c18f021b9c66f562ddc8f3d8af33fa06c9
StarwinM <mirashamzin05@gmail.com>
2024-12-22 22:31:52 +0500

    init commit

---
+++ cmd/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "github.com/StarwinM/go_final_sprint1/internal/application"
+
+func main() {
+	app := application.New()
+	app.RunServer()
+}
---
+++ go.mod
@@ -0,0 +1,1 @@
+module github.com/StarwinM/go_final_sprint1
